ccacf1d0c8d867b16f09b8706d7b962732943030
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2021-04-28 23:46:03 -0400

    added post

--- src/routers/authors.py
+++ src/routers/authors.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
-from src.models import author_table, database
+from fastapi import APIRouter, Depends
+from src.models import author_table, database, CreateAuthor
+from src.dependencies import get_current_user
+
 router = APIRouter()
 
 @router.get('/')
@@ -7,10 +9,14 @@ async def get_all_authors():
     query = author_table.select()
     return await database.fetch_all(query)
 
-
-
 @router.get('/{author_id}')
 async def get_author(author_id: int):
     query = author_table.select().where(author_table.c.id == author_id)
     return await database.fetch_all(query)
 
+
+@router.post('/authors')
+async def create_author(create_author: CreateAuthor, current_user=Depends(get_current_user)):
+    query = author_table.insert().values(name=create_author.name)
+    last_record_id = await database.execute(query)
+    return {**create_author.dict(), 'id': last_record_id}
