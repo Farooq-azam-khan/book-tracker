f80a1ed3d536a9a17c227a0f7bfc14c36d9f1c71
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2021-05-01 03:16:50 -0400

    comiple tailwindcss if enabled is true in dev

--- src/elm-ui/dev_make.sh
+++ src/elm-ui/dev_make.sh
@@ -1,1 +1,12 @@
+# if tailwindcss.config.js purge is enabled
+# set it to false and build 
+
+STR="$(grep "enabled" tailwind.config.js)"
+SUB='true'
+if [[ "$STR" == *"$SUB"* ]]; then
+    echo "Rebuilding without purgin"
+    sed -i "s/enabled: true/enabled: false/g" tailwind.config.js
+    npm run build:tailwind
+fi
+
 elm make src/Main.elm --debug --output=elm.js
