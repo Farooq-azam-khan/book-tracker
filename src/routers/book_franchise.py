898911cc6accd24e7b13edbcecd11a0ea782b6af
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2021-05-16 19:49:27 -0400

    franchise

--- src/routers/book_franchise.py
+++ src/routers/book_franchise.py
@@ -1,28 +1,26 @@
 from fastapi import APIRouter
-from src.models import (author_table, 
-                        database, 
+from src.models import (database, 
                         book_franchise_table, 
-                        CreateFranchise, 
-                        Franchise)
+                        CreateFranchise)
 from fastapi import Depends
-
-
+from src.types import Franchise
+from typing import List
 from src.dependencies import get_current_user
 
 router = APIRouter()
 
 
-@router.get('/')
+@router.get('/', response_model=List[Franchise])
 async def get_book_franchises():
     query = book_franchise_table.select()
     return await database.fetch_all(query)
 
-@router.get('/{franchise_id}')
+@router.get('/{franchise_id}', response_model=Franchise)
 async def get_a_book_franchise(franchise_id: int):
     query = book_franchise_table.select().where(book_franchise_table.c.id == franchise_id)
-    return await database.fetch_all(query)
+    return await database.fetch_one(query)
 
-@router.post('/')
+@router.post('/', response_model=Franchise)
 async def create_a_franchise(create_franchise: CreateFranchise, current_user=Depends(get_current_user)):
     query = book_franchise_table.insert().values(name=create_franchise.name)
     last_record_id = await database.execute(query)
