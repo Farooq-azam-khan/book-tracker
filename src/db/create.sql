78cff7d785610bbeb5a935f0f32c963a372c07cb
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2021-07-04 18:14:51 -0400

    updated book schema

--- src/db/create.sql
+++ src/db/create.sql
@@ -1,14 +1,8 @@
-CREATE TABLE genre (
-    id SERIAL PRIMARY KEY,
-    name TEXT UNIQUE NOT NULL
-);
-
 CREATE TABLE author (
     id SERIAL PRIMARY KEY, 
     name TEXT UNIQUE NOT NULL
 );
 
--- CREATE TYPE BookType AS ENUM ('Fiction', 'Non-Fiction');
 
 CREATE TABLE book (
     id SERIAL PRIMARY KEY, 
@@ -16,12 +10,9 @@ CREATE TABLE book (
     total_pages INTEGER DEFAULT 1 NOT NULL CHECK (total_pages >= 1), 
     total_chapters INTEGER DEFAULT 1 NOT NULL CHECK (total_chapters >= 1),
     author INTEGER REFERENCES author(id) NOT NULL, 
-    times_read INTEGER DEFAULT 0 CHECK (times_read >= 0)
-);
-
-CREATE TABLE book_genre (
-    book INTEGER REFERENCES book(id),
-    genre INTEGER REFERENCES genre(id)
+    times_read INTEGER DEFAULT 0 NOT NULL CHECK (times_read >= 0),
+    genre text[] not null default '{}',
+    table_of_contents JSON[] not null default '{}'
 );
 
 CREATE TABLE franchise (
@@ -30,19 +21,12 @@ CREATE TABLE franchise (
 );
 
 CREATE TABLE book_franchise (
-    book INTEGER REFERENCES book(id) NOT NULL ON CASCASE DELETE,
+    book INTEGER REFERENCES book(id) ON DELETE CASCADE NOT NULL,
     book_order INTEGER CHECK (book_order >= 0) NOT NULL,
-    franchise INTEGER REFERENCES franchise(id) NOT NULL ON CASCASE DELETE, 
+    franchise INTEGER REFERENCES franchise(id) ON DELETE CASCADE NOT NULL,  
     PRIMARY KEY (book, franchise, book_order)
 );
 
-CREATE TABLE book_chapters (
-    book INTEGER REFERENCES book(id),
-    chapter_name TEXT,
-    order INTEGER,
-    on_page INTEGER
-
-);
 
 CREATE TABLE history (
     book INTEGER REFERENCES book(id) NOT NULL,
---
+++ src/db/drop.sql
@@ -0,0 +1,5 @@
+    drop table history;
+    drop table book_franchise;
+    drop table franchise;
+    drop table book;
+    drop table author;
