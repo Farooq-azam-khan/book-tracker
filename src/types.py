bed21ac52b2d36ec584ed5f3e9d6acaad7cd69e4
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2021-07-04 18:15:58 -0400

    pycharm formatting

--- src/types.py
+++ src/types.py
@@ -1,49 +1,52 @@
 from pydantic import BaseModel, validator
 from typing import Optional
 # import datetime 
-from typing import List 
+from typing import List
+
 
 class User(BaseModel):
-    username: str 
-    password: str 
+    username: str
+    password: str
+
 
 class Token(BaseModel):
-    access_token: str 
-    token_type: str 
+    access_token: str
+    token_type: str
+
 
 class TokenData(BaseModel):
-    username: Optional[str] = None 
-
+    username: Optional[str] = None
 
 
 class TableOfContent(BaseModel):
-    name: str 
-    page: int 
+    name: str
+    page: int
+
 
 class CreateBook(BaseModel):
     name: str
     total_pages: int
     total_chapters: int
     author: int
-    genres: Optional[List[int]]
-    contents: Optional[List[TableOfContent]]
-    read_amount: Optional[int]
+    genre: List[str]
+    table_of_contents: List[TableOfContent]
+    times_read: int
 
     @validator('total_pages')
     def total_pages_positive(cls, v):
         if v <= 0:
             raise ValueError('total pages has to be bigger than 1')
-        return v 
+        return v
 
     @validator('total_chapters')
     def total_chapters_positive(cls, v):
         if v <= 0:
             raise ValueError('total chapters has to be bigger than 1')
         return v
-    
-    
-class Book(CreateBook): 
-    id: int 
+
+
+class Book(CreateBook):
+    id: int
 
 
 class UpdateBook(CreateBook):
@@ -53,46 +56,48 @@ class UpdateBook(CreateBook):
     author: Optional[int]
 
 
-
 class CreateFranchise(BaseModel):
-    name: str 
+    name: str
+
 
 class Franchise(CreateFranchise):
-    id: int 
+    id: int
+
 
 class CreateGenre(BaseModel):
-    name: str 
+    name: str
+
 
 class Genre(CreateGenre):
-    id: int 
+    id: int
+
 
 class CreateAuthor(BaseModel):
-    name: str 
-    nationality: Optional[str]
+    name: str
+
 
 class UpdateAuthor(CreateAuthor):
-    name: Optional[str]
+    pass
 
 
 class Author(CreateAuthor):
-    id: int 
+    id: int
 
 
 class CreateHistory(BaseModel):
-    book: int 
+    book: int
     page_mark: int
     chapter_mark: int
     read_at: Optional[str]
-    
 
     # TODO: validate page_mark and chapter_mark 
     # against previous entries
     # keep in mind that the book might have been read before 
     # and it is being read a second(or third, etc.) time 
 
+
 class History(BaseModel):
-    id: int 
-    book: int 
+    id: int
+    book: int
     start_page: Optional[int]
     end_page: int
-
